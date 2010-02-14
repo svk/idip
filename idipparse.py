21ba524ccef337ac1e4423d50c1cc84d655ce4cc
kaw <kaw.dev@gmail.com>
2010-02-14 07:29:19 +0100

    Pretty good parser
    Some ideas on how to proceed on the level above characters
    to tokens.

--- idipparse.py
+++ idipparse.py
@@ -1,3 +1,14 @@
+
+# Long or short names can be used interchangeably.
+# All army/fleet prefixes are optional.
+# Patterns for hold orders:
+#    A Mun-Holds
+#    A Mun H
+#    A Mun
+# Patterns for movement orders:
+#    A Mun-Sil
+# Patterns for 
+
 class Token:
     def __init__(self, kind, argument = None, discard = False):
         self.kind = kind
@@ -26,8 +37,8 @@ class Parser:
             suffixTokens = self.scan( suffix )
             if suffixTokens == None:
                 continue
-            if rv:
-                return None
+            # stop if rv here to eliminate ambiguity
+            # this way resolves in favour of longer tokens
             if not token.discard:
                 rv = [token] + suffixTokens
             else:
@@ -45,10 +56,20 @@ class DiplomacyParser (Parser):
             self.add( province.displayName, token )
             for alternate in province.alternateNames:
                 self.add( alternate, token )
+            for coastName in province.coasts.keys():
+                coastName = str( coastName )
+                if coastName != None:
+                    # note multiple coasts with same name map to same token
+                    # a coast token is a specifier, not a node
+                    token = Token( 'named-coast', coastName )
+                    self.addBracketed( coastName, token )
         for nation in self.board.nations:
             token = Token( 'nation', nation )
             self.add( nation.name, token )
         self.add( "-", Token( 'separator' ) )
+        self.add( "--", Token( 'separator' ) )
+        self.add( "to", Token( 'separator' ) )
+        self.add( "->", Token( 'separator' ) )
         self.add( " ", Token( 'blank', discard = True ) )
         self.add( "A", Token( 'unit', 'army' ) )
         self.add( "F", Token( 'unit', 'fleet' ) )
@@ -56,12 +77,39 @@ class DiplomacyParser (Parser):
         self.add( "convoys", Token( 'order', 'convoy' ) )
         self.add( "convoy", Token( 'order', 'convoy' ) )
         self.add( "S", Token( 'order', 'support' ) )
+        self.add( "H", Token( 'order', 'hold' ) )
+        self.add( "holds", Token( 'order', 'hold' ) )
+        self.add( "hold", Token( 'order', 'hold' ) )
         self.add( "supports", Token( 'order', 'support' ) )
         self.add( "support", Token( 'order', 'support' ) )
-        self.add( "convoyed", Token( 'order', 'convoyed' ) )
-        self.add( "(convoyed)", Token( 'order', 'convoyed' ) )
-        self.add( "by convoy", Token( 'order', 'convoyed' ) )
-        self.add( "(by convoy)", Token( 'order', 'convoyed' ) )
+        self.add( "build", Token( 'order', 'build' ) )
+        self.add( "disband", Token( 'order', 'disband' ) )
+        self.add( "disbands", Token( 'order', 'disband' ) )
+        self.add( "destroy", Token( 'order', 'disband' ) )
+        self.add( "destroyed", Token( 'order', 'disband' ) )
+        self.addBracketed( "convoyed", Token( 'order', 'convoyed' ) )
+        self.addBracketed( "by convoy", Token( 'order', 'convoyed' ) )
+        self.addStandardCoastSynonyms()
+    def addStandardCoastSynonyms(self):
+        for direction in [ "north", "east", "west", "south" ]:
+            letter = direction[0]
+            token = Token( 'named-coast', letter + "c" )
+            self.addBracketed( "{direction} coast".format( direction = direction ), token )
+            self.addBracketed( "{direction}coast".format( direction = direction ), token )
+            self.addBracketed( "{direction}".format( direction = direction ), token )
+            self.addBracketed( "{letter}. coast".format( letter = letter ), token )
+            self.addBracketed( "{letter} coast".format( letter = letter ), token )
+            self.addBracketed( "{letter}.c.".format( letter = letter ), token )
+            self.addBracketed( "{letter}. c.".format( letter = letter ), token )
+            self.addBracketed( "{letter} c".format( letter = letter ), token )
+            self.addBracketed( "{letter}c".format( letter = letter ), token )
+    def addBracketed(self, phrase, token, noPlain = False):
+        if not noPlain:
+            self.add( phrase, token )
+        self.add( "({s})".format( s = phrase ), token )
+        self.add( "[{s}]".format( s = phrase ), token )
+        self.add( "[{s}]".format( s = phrase ), token )
+        
 
 if __name__ == '__main__':
     from idipmap import createStandardBoard
